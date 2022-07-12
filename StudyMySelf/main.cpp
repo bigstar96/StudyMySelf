ff4edc7560b48924bc4f8b9d04113f03c6f2ef13
KT HAN <bigstarone96@gmail.com>
2022-07-13 00:05:37 +0900

    step 1

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -2,8 +2,23 @@
 
 int main()
 {
-    int x;
-    x = 10;
+    int alpha{ 255 }, red{ 255 }, green{ 0 }, blue{ 0 };
 
-    std::cout << x << " : " << &x << std::endl;
+    unsigned int color;
+
+    color = alpha << 24 | red << 16 | green << 8 | blue;
+
+    std::cout << std::hex;
+    std::cout << color << std::endl;
+
+    alpha = color >> 24;
+    red = color >> 16 & 0xFF;
+    green = color >> 8 & 0xFF;
+    blue = color & 0xFF;
+
+    std::cout << alpha << ", "
+        << red << ", "
+        << green << ", "
+        << blue << std::endl;
+
 }
