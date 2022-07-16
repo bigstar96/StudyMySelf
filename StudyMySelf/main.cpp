7a05de6c91f8fc14e95c2006238dae269df43a33
KT HAN <bigstarone96@gmail.com>
2022-07-16 17:53:27 +0900

    절대값

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -2,23 +2,19 @@
 
 int main()
 {
-    int alpha{ 255 }, red{ 255 }, green{ 0 }, blue{ 0 };
+    int inputNumber;
+    std::cout << "숫자를 입력하세요 : ";
+    std::cin >> inputNumber;
 
-    unsigned int color;
+    int absoluteNumber;
+    if (inputNumber < 0)
+    {
+        absoluteNumber = -inputNumber;
+    }
+    else
+    {
+        absoluteNumber = inputNumber;
+    }
 
-    color = alpha << 24 | red << 16 | green << 8 | blue;
-
-    std::cout << std::hex;
-    std::cout << color << std::endl;
-
-    alpha = color >> 24;
-    red = color >> 16 & 0xFF;
-    green = color >> 8 & 0xFF;
-    blue = color & 0xFF;
-
-    std::cout << alpha << ", "
-        << red << ", "
-        << green << ", "
-        << blue << std::endl;
-
+    std::cout << "절대값 = " << absoluteNumber;
 }
