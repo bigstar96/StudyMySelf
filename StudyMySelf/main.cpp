1fcd2da0c389bcb5bd53978cd07930f194b54721
KT HAN <bigstarone96@gmail.com>
2022-07-16 18:35:50 +0900

    bmi 계산

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -2,139 +2,37 @@
 
 int main()
 {
-    while (true)
-    {
-        int age;
-        int payment;
-        int distance;
-        std::cout << "나이를 입력하세요 : ";
-        std::cin >> age;
+	double height;
+	double weight;
+	double bmi;
 
-        std::cout << "결제 수단을 입력하세요 ( 0 : 교통카드, 1 : 일반 ) : ";
-        std::cin >> payment;
+	std::cout << "키(cm)를 입력하세요 : ";
+	std::cin >> height;
+	std::cout << "몸무게(kg)를 입력하세요 : ";
+	std::cin >> weight;
+	
+	// bmi = 몸무게 / (키 * 키)
+	// 몸무게(kg), 키(cm)
 
-        std::cout << "거리를 입력하세요 : ";
-        std::cin >> distance;
+	height /= 100;
+	bmi = weight / (height * height);
 
-        if (payment == 0)
-        {
-            if (distance < 10)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age >= 6 && age < 13)
-                {
-                    std::cout << "450 WON\n";
-                }
-                else if (age >= 13 && age <= 18)
-                {
-                    std::cout << "720 WON\n";
-                }
-                else if (age > 19)
-                {
-                    std::cout << "1250 WON\n";
-                }
-            }
-            else if (distance < 15)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age >= 6 && age < 13)
-                {
-                    std::cout << "500 WON\n";
-                }
-                else if (age >= 13 && age <= 18)
-                {
-                    std::cout << "800 WON\n";
-                }
-                else if (age > 19)
-                {
-                    std::cout << "1350 WON\n";
-                }
-            }
-            else if (distance < 20)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age >= 6 && age < 13)
-                {
-                    std::cout << "550 WON\n";
-                }
-                else if (age >= 13 && age <= 18)
-                {
-                    std::cout << "880 WON\n";
-                }
-                else if (age > 19)
-                {
-                    std::cout << "1450 WON\n";
-                }
-            }
-            else
-            {
-                std::cout << "잘못된 거리입니다.\n";
-            }
-        }
-        else if (payment == 1)
-        {
-            if (distance < 10)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age < 13 && age >= 6)
-                {
-                    std::cout << "450 WON\n";
-                }
-                else if (age >= 13)
-                {
-                    std::cout << "1350 WON\n";
-                }
-            }
-            else if (distance < 15)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age < 13 && age >= 6)
-                {
-                    std::cout << "500 WON\n";
-                }
-                else if (age >= 13)
-                {
-                    std::cout << "1450 WON\n";
-                }
-            }
-            else if (distance < 20)
-            {
-                if (age < 6)
-                {
-                    std::cout << "Free\n";
-                }
-                else if (age < 13 && age >= 6)
-                {
-                    std::cout << "550 WON\n";
-                }
-                else if (age >= 13)
-                {
-                    std::cout << "1550 WON\n";
-                }
-            }
-            else
-            {
-                std::cout << "잘못된 거리입니다.\n";
-            }
-        }
-        else
-        {
-            std::cout << "잘못된 결제 수단입니다.\n";
-        }
-    }
+	std::cout << "키 : " << height << ", 몸무게 : " << weight << "에 대한 bmi : " << bmi << std::endl;
+
+	if (bmi <= 18.5)
+	{
+		std::cout << "저체중입니다." << std::endl;
+	}
+	else if (bmi < 25)
+	{
+		std::cout << "정상입니다." << std::endl;
+	}
+	else if (bmi < 30)
+	{
+		std::cout << "과체중입니다." << std::endl;
+	}
+	else
+	{
+		std::cout << "비정상입니다." << std::endl;
+	}
 }
