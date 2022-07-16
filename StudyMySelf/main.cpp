4200b056489766bd1190dd065ae0c35b02e000c9
KT HAN <bigstarone96@gmail.com>
2022-07-16 22:20:46 +0900

    연습문제

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,38 +1,158 @@
 ﻿#include <iostream>
 
+void Print1to5()
+{
+	int i = 1;
+	while (i <= 5)
+	{
+		std::cout << i++ << std::endl;
+	}
+	std::cout << "\n";
+}
+
+void PrintOddNumber()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		if (i & 1)
+		{
+			std::cout << i << std::endl;
+		}
+		i++;
+	}
+	std::cout << "\n";
+}
+
+void PrintAtoZ()
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		std::cout << alphabet++ << " ";
+	}
+	std::cout << "\n\n";
+}
+
+void PrintMultiplicationTable()
+{
+	int a{ 2 }, b{ 0 };
+	while (b < 9)
+	{
+		std::cout << a << " X " << ++b << " = " << a * b << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void PrintMultiplicationTable2()
+{
+	int a{}, b{};
+	std::cout << "몇 단을 출력할까요? : ";
+	std::cin >> a;
+	while (b < 9)
+	{
+		std::cout << a << " X " << ++b << " = " << a * b << std::endl;
+	}
+}
+
+void PrintMultiplicationTableAll()
+{
+	for (int i = 2; i < 9; ++i)
+	{
+		for (int j = 2; j < 9; ++j)
+		{
+			std::cout << i << " X " << j << " = " << i * j << std::endl;
+		}
+		std::cout << "\n";
+	}
+}
+
+void Dice()
+{
+	for (int i = 1; i < 6; i++)
+	{
+		for (int j = 1; j < 6; j++)
+		{
+			if (i + j == 6)
+			{
+				std::cout << i << ", " << j << std::endl;
+			}
+		}
+	}
+}
+
+void UPandDOWN()
+{
+	int answer{ 6 };
+	int whatAnswer{};
+	while (whatAnswer != answer)
+	{
+		std::cout << "입력 : ";
+		std::cin >> whatAnswer;
+		if (whatAnswer < answer)
+		{
+			std::cout << "더 큰 수를 입력하세요." << std::endl;
+		}
+		else if (whatAnswer > answer)
+		{
+			std::cout << "더 작은 수를 입력하세요." << std::endl;
+		}
+		else if (whatAnswer == answer)
+		{
+			std::cout << "정답!" << std::endl;
+			break;
+		}
+	}
+}
+
+void Rectangle()
+{
+	int a, b;
+	std::cout << "너비를 입력하세요 : ";
+	std::cin >> a;
+	std::cout << "높이를 입력하세요 : ";
+	std::cin >> b;
+
+	for (int i = 0; i < b; i++)
+	{
+		for (int j = 0; j < a; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+}
+
+void Tree()
+{
+	for (int i = 1; i <= 5; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			std::cout << j;
+		}
+		std::cout << "\n";
+	}
+}
+
+void RealTree()
+{
+	
+}
+
 int main()
 {
-	double height;
-	double weight;
-	double bmi;
+	Print1to5();
+	PrintOddNumber();
+	PrintAtoZ();
+	PrintMultiplicationTable();
+	PrintMultiplicationTable2();
+	PrintMultiplicationTableAll();
+	Dice();
+	UPandDOWN();
+	Rectangle();
+	Tree();
 
-	std::cout << "키(cm)를 입력하세요 : ";
-	std::cin >> height;
-	std::cout << "몸무게(kg)를 입력하세요 : ";
-	std::cin >> weight;
-	
-	// bmi = 몸무게 / (키 * 키)
-	// 몸무게(kg), 키(cm)
-
-	height /= 100;
-	bmi = weight / (height * height);
-
-	std::cout << "키 : " << height << ", 몸무게 : " << weight << "에 대한 bmi : " << bmi << std::endl;
-
-	if (bmi <= 18.5)
-	{
-		std::cout << "저체중입니다." << std::endl;
-	}
-	else if (bmi < 25)
-	{
-		std::cout << "정상입니다." << std::endl;
-	}
-	else if (bmi < 30)
-	{
-		std::cout << "과체중입니다." << std::endl;
-	}
-	else
-	{
-		std::cout << "비정상입니다." << std::endl;
-	}
 }
