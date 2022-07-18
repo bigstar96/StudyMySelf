aa5e1f2071c2ac6b157c316629011ebe26c9d24b
KT HAN <bigstarone96@gmail.com>
2022-07-19 07:55:24 +0900

    study array

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,158 +1,137 @@
 ﻿#include <iostream>
 
-void Print1to5()
-{
-	int i = 1;
-	while (i <= 5)
-	{
-		std::cout << i++ << std::endl;
-	}
-	std::cout << "\n";
-}
-
-void PrintOddNumber()
-{
-	int i = 1;
-	while (i <= 10)
-	{
-		if (i & 1)
-		{
-			std::cout << i << std::endl;
-		}
-		i++;
-	}
-	std::cout << "\n";
-}
-
-void PrintAtoZ()
-{
-	char alphabet = 'a';
-
-	while (alphabet <= 'z')
-	{
-		std::cout << alphabet++ << " ";
-	}
-	std::cout << "\n\n";
-}
-
-void PrintMultiplicationTable()
-{
-	int a{ 2 }, b{ 0 };
-	while (b < 9)
-	{
-		std::cout << a << " X " << ++b << " = " << a * b << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void PrintMultiplicationTable2()
-{
-	int a{}, b{};
-	std::cout << "몇 단을 출력할까요? : ";
-	std::cin >> a;
-	while (b < 9)
-	{
-		std::cout << a << " X " << ++b << " = " << a * b << std::endl;
-	}
-}
-
-void PrintMultiplicationTableAll()
-{
-	for (int i = 2; i < 9; ++i)
-	{
-		for (int j = 2; j < 9; ++j)
-		{
-			std::cout << i << " X " << j << " = " << i * j << std::endl;
-		}
-		std::cout << "\n";
-	}
-}
-
-void Dice()
-{
-	for (int i = 1; i < 6; i++)
-	{
-		for (int j = 1; j < 6; j++)
-		{
-			if (i + j == 6)
-			{
-				std::cout << i << ", " << j << std::endl;
-			}
-		}
-	}
-}
-
-void UPandDOWN()
-{
-	int answer{ 6 };
-	int whatAnswer{};
-	while (whatAnswer != answer)
-	{
-		std::cout << "입력 : ";
-		std::cin >> whatAnswer;
-		if (whatAnswer < answer)
-		{
-			std::cout << "더 큰 수를 입력하세요." << std::endl;
-		}
-		else if (whatAnswer > answer)
-		{
-			std::cout << "더 작은 수를 입력하세요." << std::endl;
-		}
-		else if (whatAnswer == answer)
-		{
-			std::cout << "정답!" << std::endl;
-			break;
-		}
-	}
-}
-
-void Rectangle()
-{
-	int a, b;
-	std::cout << "너비를 입력하세요 : ";
-	std::cin >> a;
-	std::cout << "높이를 입력하세요 : ";
-	std::cin >> b;
-
-	for (int i = 0; i < b; i++)
-	{
-		for (int j = 0; j < a; j++)
-		{
-			std::cout << "*";
-		}
-		std::cout << "\n";
-	}
-	std::cout << "\n";
-}
-
-void Tree()
-{
-	for (int i = 1; i <= 5; i++)
-	{
-		for (int j = 1; j <= i; j++)
-		{
-			std::cout << j;
-		}
-		std::cout << "\n";
-	}
-}
-
-void RealTree()
-{
-	
-}
-
 int main()
 {
-	Print1to5();
-	PrintOddNumber();
-	PrintAtoZ();
-	PrintMultiplicationTable();
-	PrintMultiplicationTable2();
-	PrintMultiplicationTableAll();
-	Dice();
-	UPandDOWN();
-	Rectangle();
-	Tree();
+	int myInteger = 1000;
+	unsigned char myCharacter = myInteger;
+
+	std::cout << myInteger << std::endl;
+	std::cout << int(myCharacter) << std::endl;
+
+
+	typedef unsigned int uint;
+	uint typedefUint = 1;
+	
+	using uint = unsigned int;
+	uint usingUint = 1;
+
+
+	enum attackType
+	{
+		PhysicalAttack,
+		MagicalAttack,
+		FixedAttack
+	};
+
+	attackType aT = PhysicalAttack;
+
+	switch (aT)
+	{
+	case PhysicalAttack:
+		break;
+	case MagicalAttack:
+		break;
+	case FixedAttack:
+		break;
+	default:
+		break;
+	}
+
+	enum Color {
+		Red = 0xFFFF0000,
+		Magenta = 0xFFFF00FF,
+		Pink = 0xFFFFC0CB
+	};
+
+	bool quest1Complete = false;
+	bool quest2Complete = false;
+	bool quest3Complete = false;
+	bool quest4Complete = false;
+	if (quest1Complete && quest3Complete)
+	{
+		// 5번 퀘스트 시작
+	}
+	// 위보다 아래가 용량을 덜 잡아먹음
+	int questComplete{ 0 };		// C++11 추가
+	questComplete |= 1;			// questComplete |= 0b0001;		0b0001 = 1
+	questComplete |= 4;			// questComplete |= 0b0100;		0b0100 = 4
+	if (questComplete & 1 && questComplete & 4)
+	{
+		// 5번 퀘스트 시작
+	}
+
+
+	struct Ability {
+		int HP = 100;
+		int MP = 100;
+		int strength = 10;
+		int magic = 10;
+		int defense = 10;
+		int magicDefense = 10;
+	};
+
+	Ability warrior{ 1000,10 };
+
+	union MyUnion
+	{
+		int i;
+		float f;
+		char c;
+	};
+	MyUnion myUnion;
+	myUnion.i = 98;
+	std::cout << myUnion.i << ", " << myUnion.f << ", " << myUnion.c << std::endl;
+	std::cout << "\n";
+
+	
+	int scores[3];
+	for (int i = 0; i < 3; ++i)
+	{
+		std::cout << "Input scores[" << i + 1 << "] : ";
+		std::cin >> scores[i];
+	}
+	for (int i = 0; i < 3; ++i)
+	{
+		std::cout << i + 1 << " : " << scores[i] << std::endl;
+	}
+	int totalScore{};
+	for (int i = 0; i < 3; ++i)
+	{
+		totalScore += scores[i];
+	}
+	std::cout << "Total : " << totalScore << std::endl;
+	std::cout << "\n";
+
+
+	int myArray[15] = {
+		0,0,1,0,0,
+		0,1,1,1,0,
+		1,1,1,1,1
+	};
+	for (int i = 0; i < 15; ++i)
+	{
+		std::cout << myArray[i];
+		if ((i + 1) % 5 == 0)
+		{
+			std::cout << "\n";
+		}
+	}
+	std::cout << "\n";
+	int myArray2[3][5] = {
+		{ 0,0,1,0,0 },
+		{ 0,1,1,1,0 },
+		{ 1,1,1,1,1 }
+	};
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << myArray2[i][j];
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+
+
 
 }
