fbf205df10850ef134201766456d20e96bc61aaf
KT HAN <bigstarone96@gmail.com>
2022-07-22 01:09:38 +0900

    Array End

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -17,37 +17,98 @@ ex) 입력값 10,40,30,50,60,70,100,20,80,90
 int main()
 {
 	// 1번 문제
-	/*	
-	int Scores[10]{};
+	
+	int Scores1[10]{};
 	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << "score [" << i + 1 << "] : ";
-		std::cin >> Scores[i];
+		std::cin >> Scores1[i];
+	}
+	int total{  };
+	for (int i = 0; i < 10; ++i)
+	{
+		total += Scores1[i];
+	}
+	std::cout << "평균 : " << total / 10;
+	std::cout << "\n\n";
+	
+
+	// 2번 문제
+	
+	int Scores2[10]{};
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << "score [" << i + 1 << "] : ";
+		std::cin >> Scores2[i];
 	}
 
 	int high{0}, low{100};
 	for (int i = 0; i < 10; ++i)
 	{
-		if (high < Scores[i])
+		if (high < Scores2[i])
 		{
-			high = Scores[i];
+			high = Scores2[i];
 		}
-		if (low > Scores[i])
+		if (low > Scores2[i])
 		{
-			low = Scores[i];
+			low = Scores2[i];
 		}
 	}
 	std::cout << "최고점 : " << high << std::endl;
 	std::cout << "최저점 : " << low << std::endl;
-	*/
+	
 
-	// 2번 문제
-	int Scores[10]{};
+	// 3번 문제
+	
+	int Scores3[10]{};
 	for (int i = 0; i < 10; ++i)
 	{
 		std::cout << "score [" << i + 1 << "] : ";
-		std::cin >> Scores[i];
+		std::cin >> Scores3[i];
+	}
+	int rank[10]{};
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			if (Scores3[i] < Scores3[j])
+			{
+				rank[i]++;
+			}
+		}
 	}
 
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << rank[i] + 1 << ", ";
+	}
+	
 
+	// 4번 문제
+	
+	int Scores4[10]{};
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << "score [" << i + 1 << "] : ";
+		std::cin >> Scores4[i];
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (Scores4[i] > Scores4[j])
+			{
+				int change{ Scores4[i] };
+				Scores4[i] = Scores4[j];
+				Scores4[j] = change;
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << Scores4[i] << ", ";
+	}
+	
 }
