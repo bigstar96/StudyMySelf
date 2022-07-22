c2fa76ade4b07b2b059a439f27fd106083db9c2f
KT HAN <bigstarone96@gmail.com>
2022-07-23 04:49:03 +0900

    함수_1

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,122 +1,63 @@
 ﻿#include <iostream>
 
+double Max(double x, double y)
+{
+	return (x > y) ? x : y;
+}
 
+int ReadValue()
+{
+	std::cout << "정수를 입력하세요 : ";
+	int x{};
+	std::cin >> x;
+
+	return x;
+}
+void WirteValue(int x)
+{
+	std::cout << "x : " << x << std::endl;
+}
+
+int Quotient(int a, int b)
+{
+	return a / b;
+}
+int Remainder(int a, int b)
+{
+	return a % b;
+}
+
+void GuGuDan(int dan)
+{
+	std::cout << "--- " << dan << "단" << " ---" << std::endl;
+
+	for (int i = 1; i <= 9; ++i)
+	{
+		std::cout << dan << " x " << i << " = " << dan * i << std::endl;
+	}
+}
 
 int main()
 {
-	// 1번 문자열을 입력받아 거꾸로 출력
-	/*
-	char name[7]{};
-	std::cin >> name;
+	std::cout << Max(2.31231, 3.111201) << std::endl;
 
-	for (int i = sizeof(name); i >= 0; i--)
+	int x{ ReadValue() };
+	WirteValue(x);
+	std::cout << "\n";
+
+	int a{ 7 };
+	int b{ 3 };
+
+	std::cout << a << " / " << b << " = " << std::endl;
+	std::cout << "몫 : " << Quotient(a, b) << std::endl;
+	std::cout << "나머지 : " << Remainder(a, b) << std::endl;
+	std::cout << "\n";
+
+	for (int i = 1; i <= 9; i++)
 	{
-		std::cout << name[i];
-	}
-	*/
-
-	// 2번 두 문자열을 입력하여 하나의 문자열로 만들어 출력합시다.
-	/*
-	char name1[7]{};
-	char name2[7]{};
-	std::cin >> name1 >> name2;
-
-	char result[sizeof(name1) + sizeof(name2)]{};
-
-	for (int i = 0; i < sizeof(name2); i++)
-	{
-		result[i] = name1[i];
-		result[sizeof(name2) + i] = name2[i];
+		GuGuDan(i);
+		std::cout << std::endl;
 	}
 
-	for (int i = 0; i < sizeof(name3); i++)
-	{
-		std::cout << name3[i];
-	}
-	*/
-
-	// 3번 문자열을 입력받아 모두 대문자로 출력하는 프로그램을 만들어 봅시다.
-	/*
-	char name4[7]{};
-	std::cin >> name4;
-
-	for (int i = 0; i < sizeof(name4)-1; i++)
-	{
-		name4[i] = name4[i] - 32;
-	}
-	for (int i = 0; i < sizeof(name4); i++)
-	{
-		std::cout << name4[i];
-	}
-	*/
-
-	// 4번 테트리스 블록을 회전해 봅시다.
-	/*
-	int block[3][3] = {
-		{0,1,0},
-		{0,1,1},
-		{0,1,0}
-	};
-	
-	int rotBlock[3][3]{};
-
-	enum Command
-	{
-		LEFT = 1,
-		RIGHT = 2,
-		EXIT = 3
-	};
-	int command{};
-	while (true)
-	{
-		for (int y = 0; y < 3; ++y)
-		{
-			for (int x = 0; x < 3; ++x)
-			{
-				std::cout << block[y][x] << " ";
-			}
-			std::cout << std::endl;
-		}
-
-		std::cout << "명령 : ";
-		std::cin >> command;
-
-		int newX{}, newY{};
-
-		for (int y = 0; y < 3; ++y)
-		{
-			for (int x = 0; x < 3; ++x)
-			{
-				if (command == LEFT)
-				{
-					newX = y;
-					newY = 2 - x;
-				}
-				else if (command == RIGHT)
-				{
-					newX = 2 - y;
-					newY = x;
-				}
-
-				rotBlock[newY][newX] = block[y][x];
-			}
-		}
-
-		for (int y = 0; y < 3; ++y)
-		{
-			for (int x = 0; x < 3; ++x)
-			{
-				block[y][x] = rotBlock[y][x];
-			}
-		}
-
-		if (command == EXIT)
-		{
-			break;
-		}
-	}
-	*/
-
-	
 
 }
