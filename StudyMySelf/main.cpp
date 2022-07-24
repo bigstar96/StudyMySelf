4c49ae377a66a45679c70d4d6f93163c3f97825c
KT HAN <bigstarone96@gmail.com>
2022-07-25 05:49:44 +0900

    va_list va_start va_arg ?

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,63 +1,21 @@
 ﻿#include <iostream>
+#include <stdarg.h>
 
-double Max(double x, double y)
+void function(int count, ...)
 {
-	return (x > y) ? x : y;
-}
-
-int ReadValue()
-{
-	std::cout << "정수를 입력하세요 : ";
-	int x{};
-	std::cin >> x;
-
-	return x;
-}
-void WirteValue(int x)
-{
-	std::cout << "x : " << x << std::endl;
-}
-
-int Quotient(int a, int b)
-{
-	return a / b;
-}
-int Remainder(int a, int b)
-{
-	return a % b;
-}
-
-void GuGuDan(int dan)
-{
-	std::cout << "--- " << dan << "단" << " ---" << std::endl;
-
-	for (int i = 1; i <= 9; ++i)
+	va_list params;
+	va_start(params, count);
+	for (int i = 0; i < count; ++i)
 	{
-		std::cout << dan << " x " << i << " = " << dan * i << std::endl;
+		std::cout << va_arg(params, int);
 	}
+	std::cout << std::endl;
 }
 
 int main()
 {
-	std::cout << Max(2.31231, 3.111201) << std::endl;
+	function(1, 1);
+	function(2, 2, 3);
 
-	int x{ ReadValue() };
-	WirteValue(x);
-	std::cout << "\n";
-
-	int a{ 7 };
-	int b{ 3 };
-
-	std::cout << a << " / " << b << " = " << std::endl;
-	std::cout << "몫 : " << Quotient(a, b) << std::endl;
-	std::cout << "나머지 : " << Remainder(a, b) << std::endl;
-	std::cout << "\n";
-
-	for (int i = 1; i <= 9; i++)
-	{
-		GuGuDan(i);
-		std::cout << std::endl;
-	}
-
-
+	return 0;
 }
