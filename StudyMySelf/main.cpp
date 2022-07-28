eb1e4bd963913246169b8290e93bf33030a66348
KT HAN <bigstarone96@gmail.com>
2022-07-29 04:17:45 +0900

    Hanoi 숫자 추가

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -2,24 +2,27 @@
 
 // Recursive Function
 
-void Move(char from, char to)
+void Move(char from, char to, int n)
 {
-	std::cout << from << "에서" << to << "로 옮깁니다." << std::endl;
+	std::cout << n << "의 원판을" << from << "에서" << to << "로 옮깁니다." << std::endl;
 }
 
 void Hanoi(int n, char from, char temp, char to)
 {
 	if (n == 1)
 	{
-		Move(from, to);
+		Move(from, to, n);
 		return;
 	}
 	Hanoi(n - 1, from, to, temp); // from -> temp
-	Move(from, to);
+	Move(from, to, n);
 	Hanoi(n - 1, temp, from, to); // temp -> to
 }
 
 int main()
 {
-	Hanoi(10, 'A', 'B', 'C'); // A -> C
+	int number;
+	std::cout << "원판의 갯수를 입력하세요 : ";
+	std::cin >> number;
+	Hanoi(number, 'A', 'B', 'C'); // A -> C
 }
