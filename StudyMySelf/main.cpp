f5eee83ea6934dc891b71d89d37b1a638bd63a73
KT HAN <bigstarone96@gmail.com>
2022-07-29 04:26:15 +0900

    count 추가 Hanoi

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -4,7 +4,9 @@
 
 void Move(char from, char to, int n)
 {
-	std::cout << n << "의 원판을" << from << "에서" << to << "로 옮깁니다." << std::endl;
+	static int count1{ 0 };
+	count1++;
+	std::cout << count1 << " : " << n << "의 원판을" << from << "에서" << to << "로 옮깁니다." << std::endl;
 }
 
 void Hanoi(int n, char from, char temp, char to)
@@ -17,12 +19,11 @@ void Hanoi(int n, char from, char temp, char to)
 	Hanoi(n - 1, from, to, temp); // from -> temp
 	Move(from, to, n);
 	Hanoi(n - 1, temp, from, to); // temp -> to
+	
 }
 
 int main()
 {
 	int number;
-	std::cout << "원판의 갯수를 입력하세요 : ";
-	std::cin >> number;
-	Hanoi(number, 'A', 'B', 'C'); // A -> C
+	Hanoi(3, 'A', 'B', 'C'); // A -> C
 }
