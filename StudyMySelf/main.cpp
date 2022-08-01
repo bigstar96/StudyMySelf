0204633492e7e926e3a20500babf7f796b80695f
KT HAN <bigstarone96@gmail.com>
2022-08-02 05:38:28 +0900

    미완 하노이

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -2,28 +2,125 @@
 
 // Recursive Function
 
-void Move(char from, char to, int n)
+bool Succes{ true };
+
+enum command
 {
-	static int count1{ 0 };
-	count1++;
-	std::cout << count1 << " : " << n << "의 원판을" << from << "에서" << to << "로 옮깁니다." << std::endl;
+	Left = 1,
+	Midle = 2,
+	Right = 3
+};
+
+const int succes[3][3]{
+		{ 0, 0, 1 },
+		{ 0, 0, 2 },
+		{ 0, 0, 3 }
+};
+
+void PrintInfo(int hanoi[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << hanoi[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	return;
 }
 
-void Hanoi(int n, char from, char temp, char to)
+void Move(int hanoi[3][3], int move1, int move2)
 {
-	if (n == 1)
+	for (int i = 0; i < 3; ++i)
 	{
-		Move(from, to, n);
-		return;
+		if (hanoi[i][move1] != 0)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (hanoi[j][move2] != 0)
+				{
+					if (hanoi[j][move2] > hanoi[j + 1][move2])
+					{
+						hanoi[j][move2] = hanoi[i][move1];
+						hanoi[i][move1] = 0;
+						return;
+					}
+				}
+
+				if (hanoi[0][move2] == 0 &&
+					hanoi[1][move2] == 0 &&
+					hanoi[2][move2] == 0)
+				{
+					hanoi[2][move2] = hanoi[i][move1];
+					hanoi[i][move1] = 0;
+					return;
+				}
+			}
+		}
 	}
-	Hanoi(n - 1, from, to, temp); // from -> temp
-	Move(from, to, n);
-	Hanoi(n - 1, temp, from, to); // temp -> to
+	
+}
+
+void Hanoi(int hanoi[3][3], char from, char temp, char to)
+{
+	
+
+	int move1{ 0 }, move2{ 0 };
+	while (Succes)
+	{
+		PrintInfo(hanoi);
+		if (hanoi == succes)
+		{
+			PrintInfo(hanoi);
+			std::cout << "정답입니다." << std::endl;
+			return;
+		}
+
+		std::cout << "어떻게 움직일까요? ";
+		std::cin >> move1 >> move2;
+
+		switch (move2)
+		{
+		case Left:
+			Move(hanoi, move1 - 1, move2 - 1);
+			break;
+
+		case Midle:
+			Move(hanoi, move1 - 1, move2 - 1);
+			break;
+
+		case Right:
+			Move(hanoi, move1 - 1, move2 - 1);
+			break;
+
+		default:
+			std::cout << "잘못 입력하셨습니다." << std::endl;
+			break;
+		}
+
+		if (hanoi == succes)
+		{
+			Succes = false;
+		}
+	}
+	
+
+	
+
+	// Hanoi(hanoi, from, to, temp); // from -> temp
+	// Move(from, to);
+	// Hanoi(hanoi, temp, from, to); // temp -> to
 	
 }
 
 int main()
 {
-	int number;
-	Hanoi(3, 'A', 'B', 'C'); // A -> C
+	int hanoi[3][3]{
+		{ 1, 0, 0 },
+		{ 2, 0, 0 },
+		{ 3, 0, 0 }
+	};
+
+	Hanoi(hanoi, 'A', 'B', 'C'); // A -> C
 }
