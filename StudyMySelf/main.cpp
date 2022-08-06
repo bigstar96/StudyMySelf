f675c097efb706d38ecf3c8c46fff0ff53c8accd
KT HAN <bigstarone96@gmail.com>
2022-08-07 07:19:49 +0900

    typedef using

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,30 +1,16 @@
 ﻿#include <iostream>
 
-int Sigma(int (*f)(int x),int m, int n)
+typedef int (*FunctionPointer)(float); // C-Type
+using FunctionPoint = int(*)(float);
+
+int MyFunction(float f)
 {
-	std::cout << "Sigma->f : " << &f << std::endl;
-
-	int sum = 0;
-	for (int k = m; k <= n; ++k)
-	{
-		sum += f(k);
-	}
-
-	return sum;
+	return 0;
 }
 
-int NormalFunction(int x)
-{
-	return x;
-}
-
-int SquareFunction(int x)
-{
-	return x * x;
-}
 
 int main()
 {
-	std::cout << Sigma(NormalFunction, 1, 10) << std::endl;
-	std::cout << Sigma(SquareFunction, 1, 10) << std::endl;
+	FunctionPointer funcPtr = MyFunction;
+	FunctionPoint Funcptr = MyFunction;
 }
