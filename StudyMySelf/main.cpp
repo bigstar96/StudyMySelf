a6e25f1b1c661ccd4f2d9e862bea41551e0adc19
KT HAN <bigstarone96@gmail.com>
2022-08-08 23:36:37 +0900

    널 문자의 필요성

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,37 +1,21 @@
 ﻿#include <iostream>	
 
+
+
 int main()
 {
-	int array[10]{ 1,2,3,4,5,6,7,8,9,10 };
+	char c[6];
+	char* pc;
 
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << &array[i] << " " << std::endl;
-	}
+	c[0] = 'd';
+	c[1] = 'o';
+	c[2] = 'g';
+	c[3] = 'g';
+	c[4] = 'y';
 
-	int* p = &array[1];
-	std::cout << p << std::endl;
-	p = array + 1;
-	std::cout << p << std::endl;
+	std::cout << c << std::endl;
 
-	int sum1{}, sum2{}, sum3{};
-	// 기존 방법
-	for (int i = 0; i < 10; ++i)
-	{
-		sum1 += array[i];
-	}
-	// 포인터 활용
-	for (int i = 0; i < 10; ++i)
-	{
-		sum2 += *(array + i);
-	}
-	// 포인터 및 for문 고급
-	for (int i = 0, *p = array; i < 10; ++i, ++p)
-	{
-		sum3 += *p;
-	}
+	pc = &c[0];
 
-
-	std::cout << sum1 << " " << sum2 << " " << sum3 << std::endl;
-
+	std::cout << pc << std::endl;
 }
