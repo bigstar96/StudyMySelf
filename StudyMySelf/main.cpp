a4381bc420654c02979d172542e9eb60d7dea1b7
KT HAN <bigstarone96@gmail.com>
2022-08-15 04:48:52 +0900

    기본 예외 처리

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,22 +1,31 @@
 ﻿#include <iostream>	
 
-double LinearEquation(int a, int b)
+double linear_equation(int a, int b)
 {
 	if (a == 0)
 	{
-		std::cerr << "divide by zero";
+		throw std::logic_error("divide by zero!");
 	}
+
 	return -((double)b / a);
 }
 
 int main()
 {
-	int a;
-	int b;
+	try
+	{
+		// your code
+		return 0;
+	}
+	catch (std::exception e)
+	{
+		std::cerr << "error : " << e.what() << std::endl;
+		return 1;
+	}
+	catch (...)
+	{
+		std::cerr << "unknown error! " << std::endl;
+		return 2;
+	}
 
-	std::cin >> a >> b;
-
-	std::cout << LinearEquation(a, b) << std::endl;
-	
-	return 0;
 }
