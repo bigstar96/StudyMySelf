d0bf3f9271b5d63e4d2ee5b2fea4a0d1596fc088
KT HAN <bigstarone96@gmail.com>
2022-08-15 23:00:14 +0900

    평균구하기

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,31 +1,37 @@
 ﻿#include <iostream>	
 
-double linear_equation(int a, int b)
+int average(int a, int b, int c)
 {
-	if (a == 0)
+	if (!(a > 0 && a < b && b < c))
 	{
-		throw std::logic_error("divide by zero!");
+		throw std::logic_error("a, b, c should positive and a < b < c");
 	}
 
-	return -((double)b / a);
+	int average{ 0 };
+	average = (double)(a + b + c) / 3.0;
+
+	if (average < 0)
+	{
+		throw std::logic_error("average is negative!");
+	}
+	return average;
 }
 
 int main()
 {
+	int a, b, c;
+
+	std::cout << "세 정수를 입력하세요 : ";
+	std::cin >> a >> b >> c;
+
 	try
 	{
-		// your code
-		return 0;
+		std::cout << average(a, b, c) << std::endl;
 	}
-	catch (std::exception e)
+	catch (std::logic_error)
 	{
-		std::cerr << "error : " << e.what() << std::endl;
-		return 1;
-	}
-	catch (...)
-	{
-		std::cerr << "unknown error! " << std::endl;
-		return 2;
+		std::cerr << "divide by zero!" << std::endl;
 	}
 
+	return 0;
 }
