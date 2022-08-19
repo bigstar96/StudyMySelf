d72b45b90013b35198b6fcdedcb670f5182a18c1
KT HAN <bigstarone96@gmail.com>
2022-08-20 00:25:07 +0900

    성능체크

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,177 +1,47 @@
 ﻿#include <iostream>
+#include <chrono>
 
-void PrintArray(int input[], int size)
+using Comparison = bool (*)(int, int);
+
+bool Asscending(int x, int y)
 {
-	for (int i = 0; i < size; ++i)
-	{
-		std::cout << input[i] << " ";
-	}
+	return x < y;
 }
 
-void Swap(int& value1, int& value2)
+bool Descending(int x, int y)
 {
-	int temp = value1;
-	value1 = value2;
-	value2 = temp;
+	return x > y;
 }
 
-void SequentialSort(int input[], int size)
+void Sort(int numbers[], int count, Comparison f)
 {
-	for (int i = 0; i < size - 1; ++i)
-	{
-		for (int j = i + 1; j < size; ++j)
-		{
-			if (input[i] > input[j])
-			{
-				Swap(input[i], input[j]);
-			}
-		}
-	}
-}
-
-void SelectionSort(int input[], int size)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		int minIndex = i;
-		for (int j = i; j < size; ++j)
-		{
-			if (input[minIndex] > input[j])
-			{
-				minIndex = j;
-			}
-		}
-		if (minIndex != i)
-		{
-			Swap(input[i], input[minIndex]);
-		}
-	}
-}
-
-void BubbleSort(int input[], int size)
-{
-	for (int phase = 0; phase < size - 1; ++phase)
-	{
-		for (int k = 0; k < size - phase - 1; ++k)
-		{
-			if (input[k] > input[k + 1])
-			{
-				Swap(input[k], input[k + 1]);
-			}
-		}
-	}
-}
-
-void InsertionSort(int input[], int size)
-{
-	for (int i = 1; i < size; ++i)
-	{
-		int j = i;
-		int target = input[i];
-
-		while (--j >= 0 && target < input[j])
-		{
-			input[j + 1] = input[j];
-			input[j] = target;
-		}
-	}
-}
-
-void Merge(int input[], int start, int half, int end, int temp[])
-{
-	int i = start;
-	int j = half + 1;
-	int tempIndex = 0;
-
-	// 왼쪽 블럭과 오른쪽 블럭을 정렬 합병
-	while (i <= half && j <= end)
-	{
-		if (input[i] < input[j])
-		{
-			temp[tempIndex++] = input[i++];
-		}
-		else
-		{
-			temp[tempIndex++] = input[j++];
-		}
-	}
-
-	// 남은 인덱스들을 병합
-	// 왼쪽 or 오른쪽 블럭이 남아 있을경우 대비해서 각각 병합
-	while (i <= half)
-	{
-		temp[tempIndex++] = input[i++];
-	}
-
-	while (j <= end)
-	{
-		temp[tempIndex++] = input[j++];
-	}
-
-	tempIndex = 0;
-
-	for (int i = start; i <= end; ++i)
-	{
-		input[i] = temp[tempIndex++];
-	}
-}
-
-void MergeSort(int input[], int start, int end, int temp[])
-{
-	if (start >= end)
-	{
-		return;
-	}
+	int temp{};
 	
-	int half = (start + end) / 2;
-
-	MergeSort(input, start, half, temp);
-	MergeSort(input, half + 1, end, temp);
-	Merge(input, start, half, end, temp);
-}
-
-void QuickSort(int input[], int left, int right)
-{
-	int i = left;
-	int j = right;
-	int pivot = input[(left + right) / 2];
-	int temp;
-	do
+	for (int i = 0; i < count; ++i)
 	{
-		while (input[i] < pivot)
+		for (int j = i + 1; j < count; ++j)
 		{
-			i++;
+			if (f(numbers[i], numbers[j]))
+			{
+				temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
+			}
 		}
-		while (input[j] > pivot)
-		{
-			j--;
-		}
-		if (i <= j)
-		{
-			Swap(input[i], input[j]);
-			i++;
-			j--;
-		}
-	} while (i <= j);
-
-	if (left < j) QuickSort(input, left, j);
-
-	if (i < right) QuickSort(input, i, right);
+	}
 }
 
 int main()
 {
-	const int size = 5;
-	int Array[size] = { 8,7,2,3,1 };
-	PrintArray(Array, size);
-	std::cout << std::endl;
+	const int NumArray = 10;
+	int scores[NumArray]{ 20,10,40,15,30,70,60,90,80,40 };
 
-	/*int tempArray[size] = {};
-	MergeSort(Array, 0, size - 1, tempArray);*/
+	auto startTime = std::chrono::system_clock::now();
+	Sort(scores, NumArray, Descending);
 
-	QuickSort(Array, 0, size - 1);
-	PrintArray(Array, size);
-	std::cout << std::endl;
+	auto endTime = std::chrono::system_clock::now();
 
+	auto duration = endTime - startTime;
 
+	std::cout << "Sort() runs : " << duration.count() << "ms" << std::endl;
 }
