7a52314d6d1b3290c648a877d81f4a5868b7bd18
KT HAN <bigstarone96@gmail.com>
2022-08-30 22:10:23 +0900

    생성자 소멸자

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,39 +1,59 @@
 ﻿#include <iostream>
 
-class Point2D
-{
-private:
-	int mX;
-	int mY;
+//class Point2D
+//{
+//private:
+//	int mX;
+//	int mY;
+//
+//public:
+//	void Set(int x, int y);
+//	double Length();
+//	void Print();
+//};
+//
+//void Point2D::Set(int x, int y)
+//{
+//	mX = x;
+//	mY = y;
+//}
+//
+//double Point2D::Length()
+//{
+//	return sqrt(mX * mX + mY * mY);
+//}
+//
+//void Point2D::Print()
+//{
+//	std::cout << "(" << mX << ", " << mY << ")" << std::endl;
+//}
 
+class MyClass
+{
 public:
-	void Set(int x, int y);
-	double Length();
-	void Print();
+	MyClass()
+	{
+		std::cout << "[Constructor]" << std::endl;
+	}
+	~MyClass()
+	{
+		std::cout << "[Destructor]" << std::endl;
+	}
+
+private:
+
 };
-
-void Point2D::Set(int x, int y)
-{
-	mX = x;
-	mY = y;
-}
-
-double Point2D::Length()
-{
-	return sqrt(mX * mX + mY * mY);
-}
-
-void Point2D::Print()
-{
-	std::cout << "(" << mX << ", " << mY << ")" << std::endl;
-}
 
 int main()
 {
-	Point2D pt;
+	/*Point2D pt;
 
 	pt.Set(3, 3);
 	pt.Print();
 
-	std::cout << pt.Length();
+	std::cout << pt.Length();*/
+
+	MyClass c1;
+
+	std::cout << "MyClass Instantiate!" << std::endl;
 }
