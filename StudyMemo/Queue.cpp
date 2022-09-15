29a8f9adbcef065ff01edd27cc202b055b42d3bf
KT HAN <bigstarone96@gmail.com>
2022-09-16 01:04:37 +0900

    Class로 Queue 만들기

--- StudyMemo/Queue.cpp
+++ StudyMemo/Queue.cpp
@@ -1,22 +1,76 @@
 #include "Queue.h"
+#include <iostream>
 
-Queue::Queue()
+Queue::Queue() : mCount{ 0 }, mFront{ nullptr }, mRear{ nullptr }
 {
 }
 
 Queue::~Queue()
 {
+    Element* element = mFront;
+    Element* next = nullptr;
+
+    while (element != nullptr)
+    {
+        next = element->next;
+        delete element;
+        element = next;
+    }
 }
 
 void Queue::Enqueue(int value)
 {
+    Element* element = new Element;
+    element->value = value;
+    element->next = nullptr;
+
+    if (mCount == 0)
+    {
+        mFront = element;
+        mRear = element;
+    }
+    else
+    {
+        mRear->next = element;
+        mRear = element;
+    }
+
+    mCount++;
 }
 
 bool Queue::Dequeue()
 {
+    if (mCount == 0)
+    {
+        std::cout << "QUEUE is EMPTY!" << std::endl;
+        return false;
+    }
+
+    Element* element = mFront;
+    
+    std::cout << "dequeue : " << element->value << std::endl;
+
+    mFront = element->next;
+
+    delete element;
+    mCount--;
+    
+    if (mCount == 0)
+    {
+        mCount = 0;
+        mFront = mRear = nullptr;
+    }
+
     return false;
 }
 
 void Queue::Print() const
 {
+    Element* element = mFront;
+
+    while (element != nullptr)
+    {
+        std::cout << element->value << std::endl;
+        element = element->next;
+    }
 }
