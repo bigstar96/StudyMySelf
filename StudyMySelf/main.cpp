4248caf5f4ddb94cca0e902fa3eeb969221f517a
KT HAN <bigstarone96@gmail.com>
2022-09-19 00:00:44 +0900

    Class - 정적 멤버 함수의 활용

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -1,8 +1,32 @@
 ﻿#include <iostream>
 
+class MyClass
+{
+public:
+	static int sCount;
 
+	MyClass()
+	{
+		sCount++;
+	}
+
+	~MyClass()
+	{
+		sCount--;
+	}
+
+private:
+
+};
+
+int MyClass::sCount{ 0 };
 
 int main()
 {
+	MyClass c1;
+	MyClass c2;
+	MyClass c3;
 
+	std::cout << c3.sCount << std::endl;
+	std::cout << MyClass::sCount << std::endl;
 }
