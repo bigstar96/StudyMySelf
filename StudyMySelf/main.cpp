b16674d54ef04fce554ceda689d3972c0b12066e
KT HAN <bigstarone96@gmail.com>
2022-09-20 01:18:36 +0900

    Class - operator 전위 후위 연산자

--- StudyMySelf/main.cpp
+++ StudyMySelf/main.cpp
@@ -15,10 +15,37 @@ public:
 		std::cout << "(" << mX << ", " << mY << ")" << std::endl;
 	}
 
-	friend Point2D operator+(Point2D& x, Point2D& y);
+	Point2D operator+(Point2D rhs)
+	{
+		Point2D result{ 0,0 };
+		result.mX = this->mX + rhs.mX;
+		result.mY = this->mY + rhs.mY;
+
+		return result;
+	}
+
+	const Point2D& operator++()
+	{
+		++mX;
+		++mY;
+
+		return *this;
+	}
+
+	const Point2D& operator++(int)
+	{
+		// 반환 후 증가
+		Point2D temp(mX, mY);
+
+		++(*this);
+
+		return temp;
+	}
+
+	friend Point2D operator+(const Point2D& x, const Point2D& y);
 };
 
-Point2D operator+(Point2D& x, Point2D& y)
+Point2D operator+(const Point2D& x, const Point2D& y)
 {
 	Point2D newPt{ 0,0 };
 
@@ -40,4 +67,10 @@ int main()
 
 	Point2D pt4 = pt1 + pt2 + pt3;
 	pt4.Print();
+
+	Point2D pt5 = ++pt1;
+	pt5.Print();
+
+	Point2D pt6 = pt1++;
+	pt6.Print();
 }
